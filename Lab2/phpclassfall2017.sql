b52da596f5bea0e187c10ec7975b863576393795
nfonseca1 <nrfonseca@email.neit.edu>
2017-10-19 14:07:01 -0400

    Lab2 export

---
+++ Lab2/phpclassfall2017.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2017 at 07:59 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpclassfall2017`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `actors`
+--
+
+CREATE TABLE `actors` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `firstname` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `dob` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `height` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `actors`
+--
+
+INSERT INTO `actors` (`id`, `firstname`, `lastname`, `dob`, `height`) VALUES
+(2, 'Will', 'Smith', '1968-09-25', '6&#39; 2&#34;'),
+(3, 'Leonardo', 'DiCaprio', '1974-11-11', '6&#39; 0&#34;'),
+(4, 'Johnny', 'Depp', '1963-06-09', '5&#39; 10&#34;'),
+(5, 'Denzel', 'Washington', '1954-12-28', '6&#39; 1&#34;'),
+(6, 'Dwayne', 'Johnson', '1972-05-02', '6&#39; 5&#34;');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `actors`
+--
+ALTER TABLE `actors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `actors`
+--
+ALTER TABLE `actors`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
